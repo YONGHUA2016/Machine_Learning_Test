29f51f251b4bb5d777c94b36c38ee63bf20774f6
YONGHUA2016 <yonghuayinimperial14@gmail.com>
2018-08-14 13:41:04 +0100

    generate random dataset to CSV

--- DataPreprocessing/DataPreprocessing.py
+++ DataPreprocessing/DataPreprocessing.py
@@ -3,9 +3,12 @@ import pandas as pd
 import tensorflow as tf
 
 fake_data = np.random.rand(1000,20)
+fake_data = pd.DataFrame(fake_data)
+dataname = 'datasest.csv'
+fake_data.to_csv(dataname)
 
-data = pd.read_csv('dataset.csv')
+data = pd.read_csv(dataname)
 
-print data.size(0)
+
 
 
